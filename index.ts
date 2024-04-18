f2cf3ef883a042b13b3a6a1b488d5674c630155c
sumeraali2009 <165699963+sumeraali2009@users.noreply.github.com>
2024-04-18 13:11:51 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+//2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+//would you like to learn some Python today?
+
+
+//let firstName : string ="Eric";
+//console.log (firstName);
+//console.log(`hello ${firstName}, "would you like to learn some Python today"?`)
+
+
+//3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.		
+
+let firstName:string = "sumera ali";
+console.log(firstName . toUpperCase());
+console.log (firstName . toLowerCase());
+console.log(firstName.charAt(0).toUpperCase()+firstName.slice(1));
